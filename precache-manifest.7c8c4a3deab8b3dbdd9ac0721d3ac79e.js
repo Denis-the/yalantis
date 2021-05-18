9b5571ba9bbdf971268001298dbb9937fd5ab745
Denis-the <d.rozumnyu@gmail.com>
2021-05-18 19:48:54 +0300

    Updates

---
+++ precache-manifest.7c8c4a3deab8b3dbdd9ac0721d3ac79e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3a9553807ac9c98186e",
+    "url": "/static/css/main.8b5d2f18.chunk.css"
+  },
+  {
+    "revision": "e3a9553807ac9c98186e",
+    "url": "/static/js/main.05a35bf8.chunk.js"
+  },
+  {
+    "revision": "b43d712b6e40ba449383",
+    "url": "/static/js/runtime~main.7196d99e.js"
+  },
+  {
+    "revision": "6f8eefa822dda993f4a3",
+    "url": "/static/js/2.ab74f0f4.chunk.js"
+  },
+  {
+    "revision": "95fb52fd931ee8cbca61",
+    "url": "/static/js/3.e2f69762.chunk.js"
+  },
+  {
+    "revision": "13d0064c7272068e8a76",
+    "url": "/static/js/4.e8e6eb84.chunk.js"
+  },
+  {
+    "revision": "76794da6b20709738034558a2468ebc4",
+    "url": "/index.html"
+  }
+];
